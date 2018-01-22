cd205b4397914bd605302dd249c772e2fb58cfdc
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-01-22 03:42:09 +0000

    added xml comments

--- src/MonkeyMask.Core/MonkeyEntry.cs
+++ src/MonkeyMask.Core/MonkeyEntry.cs
@@ -5,6 +5,33 @@ namespace MonkeyMask.Core
 {
     public static class MonkeyEntry
     {
+        /// <summary>
+        /// Mask the input based on the provided format.
+        /// </summary>
+        /// <remarks>
+        /// ONLY SUPPORTS NUMERIC INPUTS AND FORMATS
+        /// Intended to be used with keyboard entry.
+        /// </remarks>
+        /// <example>
+        /// // partial mask
+        /// var input = "123-4";
+        /// var format = "###-####";
+        /// MonkeyEntry.Mask(input, format);
+        ///
+        /// // full mask
+        /// var input = "123-4567";
+        /// var format = "###-####";
+        /// MonkeyEntry.Mask(input, format);
+        ///
+        /// // full mask and limit a max length
+        /// var input = "123-4567";
+        /// var format = "###-####";
+        /// MonkeyEntry.Mask(input, format, 7);
+        /// </example>
+        /// <param name="input">Unmasked string</param>
+        /// <param name="format">Formatting pattern</param>
+        /// <param name="maxLength">Max length of output</param>
+        /// <returns>Masked input</returns>
         public static string Mask(string input, string format, int maxLength = 0)
         {
             string output = null;
