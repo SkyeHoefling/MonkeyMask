1bfbbbe8b970736cc981f6c1b7f5a4d418ad9ff4
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-01-22 02:37:47 +0000

    Added workflow test to simulate real world example

---
+++ tests/MonkeyMask.Core.Tests/WorkflowTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace MonkeyMask.Core.Tests
+{
+    [TestFixture]
+    public class WorkflowTests
+    {
+        [Test]
+        public void SimpleEntryFieldTest()
+        {
+            var format = "##-##";
+            var input = string.Empty;
+
+            var result = MonkeyEntry.Mask(input, format);
+            Assert.IsEmpty(result);
+
+            input = result + "1";
+            result = MonkeyEntry.Mask(input, format);
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual("1", result);
+
+            input = result + "1";
+            result = MonkeyEntry.Mask(input, format);
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual("11-", result);
+
+            input = result + "1";
+            result = MonkeyEntry.Mask(input, format);
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual("11-1", result);
+
+            input = result + "1";
+            result = MonkeyEntry.Mask(input, format);
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual("11-11", result);
+        }
+    }
+}
